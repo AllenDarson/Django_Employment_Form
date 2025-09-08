2ac64302f280ba716f5898cf590db78b7979d875
Allen Darson <allendarson27@gmail.com>
2025-09-08 17:16:18 +0530

    Updated Django project files

--- sql_employment/settings.py
+++ sql_employment/settings.py
@@ -73,14 +73,20 @@ WSGI_APPLICATION = 'sql_employment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'employment_django',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
 DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'employment_django',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
